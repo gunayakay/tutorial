9b221b486d41e7566f58e61a85e289602561f812
Gunay Akay <gunayakay@outlook.com>
2022-12-25 18:36:55 +0300

    Navigation is added between screens

--- src/pages/company/history.js
+++ src/pages/company/history.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const History = () => {
-  return <div>History</div>;
+  return (
+    <div>
+      <h2>History</h2>
+      <Link to="/">Home</Link>
+    </div>
+  );
 };
 
 export default History;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,17 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
 export default function Home() {
   return (
     <div>
-      <p>Hello world!</p>
+      <h1>Hello world!</h1>
+      {/* Difference between Link and a  Link is not refreshing page but a is doing that why we are usinge Link in gatsby   */}
+      <div>
+        <Link to="/about">About</Link>
+      </div>
+      <div>
+        <Link to="/company/history">History</Link>
+      </div>
     </div>
   );
 }
