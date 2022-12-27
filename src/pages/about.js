135695ad600328dd019459d045cf53c10ec539c9
Gunay Akay <gunayakay@outlook.com>
2022-12-27 15:56:13 +0300

    about page is completed

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 function About() {
   return (
     <Layout>
-      <h2>About Page</h2>
+      <main className="page">
+        <section className="about-page">
+          <article className="">
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt in Bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   );
 }
