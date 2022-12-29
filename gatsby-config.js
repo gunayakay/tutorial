aa1a33b36297cf3344d6b458c6b9183360b383dc
Gunay Akay <gunayakay@outlook.com>
2022-12-29 15:27:46 +0300

    index page is fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,28 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata: {
+    title: `Simply Recipes`,
+    description: `Nice and clean recipes site`,
+    author: `@gunayakay`,
+    person: { name: "Günay", age: 26 },
+    simpleData: ["item1", "item2"],
+    complexData: [
+      { name: "Günay", age: 26 },
+      { name: "Simge", age: 24 },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 function Home() {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>Simply recipes</h1>
+              <h4>no fluff, just recipes</h4>
+            </div>
+          </div>
+        </header>
+      </main>
       {/* Difference between Link and a  Link is not refreshing page but a is doing that why we are usinge Link in gatsby   */}
     </Layout>
   );
